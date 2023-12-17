af2b548f9cb1b166792f02ad904a5e56952e2d98
Payal Rathod <76880045+payalrathod@users.noreply.github.com>
2023-12-17 18:31:19 +0530

    Create Loops.js

---
+++ Loops.js
@@ -0,0 +1,25 @@
+//loops
+
+console.log('Hello World!')
+
+//for loop
+for(let i=0; i<5; i++)  {
+  console.log('Hello World '+i)
+}
+
+var cars = ["Volvo","Toyota","Tesla"]
+for(let car of cars)  {
+  console.log(car)
+  if(car == "Toyota")  {
+    break
+  }
+}
+
+//ES6 syntax for each loop
+cars.forEach(car => {
+  console.log(car)
+})
+
+
+
+
