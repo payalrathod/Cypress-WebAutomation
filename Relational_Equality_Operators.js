af54770b77d36269303bd4e70f43899d65683153
Payal Rathod <76880045+payalrathod@users.noreply.github.com>
2023-12-17 15:48:55 +0530

    Create Relational_Equality_Operators.js

---
+++ Relational_Equality_Operators.js
@@ -0,0 +1,22 @@
+//Relational or comparison operators
+
+// > -more than
+// < -less than
+// >= -more than equal
+// <= -less than equal
+
+var result = 10 > 5
+console.log(result)  //true
+var result = 10 < 5
+console.log(result)  //false
+var result = 5 > 5
+console.log(result)  //false
+var result = 5 >= 5
+console.log(result)  //true
+var result = 6 >= 5
+console.log(result)  //true
+
+//equality operators
+var = 1
+console.log(x == '1')  //lose comparison
+console.log(x === '1')  //strict comparison
