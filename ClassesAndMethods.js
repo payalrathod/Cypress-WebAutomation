3ac12f28716fe480e539c3e34ac6acb1de48504d
Payal Rathod <76880045+payalrathod@users.noreply.github.com>
2023-12-19 22:32:14 +0530

    Update ClassesAndMethods.js

--- ClassesAndMethods.js
+++ ClassesAndMethods.js
@@ -1,1 +1,6 @@
+//import {CustomerDetails} from "../folderName/printHelper.js";
+import {customerDetails} from "../folderName/printHelper.js";
 
+//var customerDetails = new CustomerDetails()
+customerDetails.printFirstName('Steve')
+customerDetails.printLastName('Smith')
